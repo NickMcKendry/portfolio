028b2592c4d933bdc73de2734f02ca16fb4025b5
Nick McKendry <nickmckendry@gmail.com>
2018-04-19 17:22:23 -0600

    updating portfolio

--- src/Cards/Projects/Projects.js
+++ src/Cards/Projects/Projects.js
@@ -3,7 +3,7 @@ import './Projects.css';
 import Revkit from './Revkit.png';
 import RevkitMods from './RevkitMods.png';
 import RevkitBuild from './RevkitBuild.png';
-import AgileOverload from './AgileOverload.png';
+import CT from './CT.png';
 import RevMatch from './RevMatch.png';
 import BTG from './Leaf.svg';
 
@@ -50,20 +50,20 @@ export default class Projects extends Component {
                     <div className="info">
                       <div className="content">
                         <h4>REVKIT Build Form</h4>
-                        <p>I was the lead dev of a team of 4 for this project, this form revolutionized the way things were done at REVKIT. This allowed user a good looking experience to subit their build for a build page. The data was then turned into a mark down file for our cms and would generate the template automatically. This turned a 2 hour task into a 5 minute task. Built using React, Node, Express, and MySql.</p>
+                        <p>I was the lead dev of a team of 4 for this project, this form revolutionized the way things were done at REVKIT. This allowed user a good looking experience to submit their build for a build page. The data was then turned into a mark down file for our cms and would generate the template automatically. This turned a 2 hour task into a 5 minute task. Built using React, Node, Express, and MySql.</p>
                       </div>
                     </div>
                   </div>
                 </a>
               </div>
               <div className="mix col-lg-4 col-md-6 col-sm-6 col-xs-12">
-                <a href="https://agile-overload.herokuapp.com/" target="_blank">
+                <a href="https://cloveandtwine.com/" target="_blank">
                   <div className="item">
-                    <img src={AgileOverload} />
+                    <img src={CT} />
                     <div className="info">
                       <div className="content">
-                        <h4>Agile Overload</h4>
-                        <p>This was my second quarter project at Galvanize and my first project working in a group and also my first project with backend. Agile Overload is a 4 player game based on agile methodologies. Built with JavaScript, Node, Express, WebSockets, and PostgreSql.</p>
+                        <h4>Clove & Twine</h4>
+                        <p>This was my first big project working as a freelancer. I helped a team build and update ths site. Clove & Twine is one of the top sellers for corporate goods. Built using Shopify, Jquery, Boostrap, HTML, liquid, JavaScript, and CSS</p>
                       </div>
                     </div>
                   </div>
